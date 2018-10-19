1d4f3a5b02d7c6a47ba59998e7c39aba8187e7bf
Tian <tianliuau@gmail.com>
2018-10-19 14:32:14 +1100

    to add comments on files  in  /server directory
    Add descriptions of module and functions in everyfile under the directory of /server

--- src/server/Auth/authCheck.js
+++ src/server/Auth/authCheck.js
@@ -1,5 +1,16 @@
+/**
+* Authentication check module
+* @module ../auth/authCheck
+* @see ../auth/authCheck
+*/
+
 var jwt = require('jsonwebtoken');
 
+/**
+ * authentic if user have the right token to login
+ * @param {string} token - the token that need to be verified.
+ * @param {function} callback - return whether the verification successful or not
+ */
 exports.tokenVerify = function(token, callback){
   jwt.verify(token, 'shhhhh', function(err, decoded) {
     if(err) {
--- src/server/dao/modulesCRUD.js
+++ src/server/dao/modulesCRUD.js
@@ -1,6 +1,16 @@
+/**
+ * operations of module include find, list, choose and like
+ * @module modulesAPI
+ * @see module: '../dao/modulesCRUD'
+ */
+
 const SQLITE3= require('sqlite3').verbose();
 var db = new SQLITE3.Database('../../CRUD.db');
 
+/**
+ * to find if module exist in the modules table in database
+ * @param callback {function} - get the module data
+ */
 exports.findModules = function(callback) {
   var sqlFindModule = `SELECT * FROM modules`;
 
@@ -13,6 +23,10 @@ exports.findModules = function(callback) {
   });
 };
 
+/**
+ * to list module ranking by the number of likes
+ * @param callback {function} - get the module data
+ */
 exports.listFavoritesModules = function(callback) {
   var sqlListFavorModules = `SELECT module_id, name, coodinator, numberlikes FROM modules ORDER BY numberlikes DESC`;
 
@@ -25,6 +39,12 @@ exports.listFavoritesModules = function(callback) {
   });
 };
 
+/**
+ * the user can click like button to support a course, every user only have one time to click that
+ * @param user_mail {string} - to get the user data by email
+ * @param module_id {string} - to get the module data by id
+ * @param callback {function} - to label the like parameter of user
+ */
 exports.likeButtonModule = function (user_mail, module_id, callback) {
   var sqlCheckUserLike = `SELECT like_status FROM students WHERE email = ?`;
   var sqlUpdateLike = `UPDATE modules SET numberlikes = numberlikes + 1 WHERE module_id = ?`;
@@ -37,8 +57,10 @@ exports.likeButtonModule = function (user_mail, module_id, callback) {
         callback(err);
       }else{
         if(like_status.like_status === 'true'){
+          // if the user had voted to the course before, he can't like vote to another course, every user only has one chance
           callback('You already vote before, only one chance', 1);
         }else if(like_status.like_status === 'false'){
+          // update DB that user already like a course
           db.run(sqlUpdateLike, [module_id], function (err) {
             if(err){
               callback(err);
@@ -60,34 +82,37 @@ exports.likeButtonModule = function (user_mail, module_id, callback) {
   }
 };
 
-// exports.AddMoudules = function (module, callback) {
-//   var sqlAddModule = `INSERT INTO modules(name,coodinator,description) VALUES(?,?,?)`;
-//   db.run(sqlAddModule, [module.name, module.coodinator, module.description], function (err) {
-//     if (err) {
-//       callback(err);
-//     }
-//     console.log('A row has been inserted with row ID');
-//     callback();
-//   });
-// };
-
+/**
+ * update database and send data to frontend when user choose a module
+ * @param userMail {string} - to get the user information
+ * @param modules {string} - to get the module information
+ * @param callback - to get the result
+ */
 exports.chooseModules = function (userMail, modules, callback) {
   var userMail = userMail;
   var modules = modules;
 
+
+  // call chosenModule method to get the module user already chose
   chosenModule(userMail, function (err, currentTimeID) {
     if (err) {
       console.log('Error callback because of wrong user mail');
     } else {
       console.log('Choose modules', currentTimeID);
+
+      //call currentModules method to get the time id of the module
       currentModules(modules, function (err, newTimeID) {
         if (err) {
           callback('Please input one module', 1);
         } else {
+          // add the newly selected course to to the user time id
           var allTimeID = currentTimeID.concat(newTimeID);
+
+          // call checkRepeat function to see the time conflict
           var result = checkRepeat(allTimeID);
 
           if (result === true) {
+            // call insertModulesId to get the module inserted into the user table
             insertModulesId(userMail, modules, function (err) {
               if (err) {
                 callback(err);
@@ -103,6 +128,11 @@ exports.chooseModules = function (userMail, modules, callback) {
     }
   });
 
+  /**
+   * to find the course user already chose.
+   * @param email {string} - get user data by email
+   * @param callback {function} - get the result of operation
+   */
   function chosenModule(email, callback) {
     var sqlChosenModule = `SELECT mt.time_id 
                FROM timetable AS t JOIN modules_time AS mt 
@@ -122,6 +152,11 @@ exports.chooseModules = function (userMail, modules, callback) {
     })
   };
 
+  /**
+   * to get the time id of current modules
+   * @param module {string} - get module information by id
+   * @param callback {function} - to get the operation result
+   */
   function currentModules(module, callback) {
     if (modules.length !== 0) {
       var sqlCurrentModule = `SELECT time_id FROM modules_time WHERE module_id = ?`;
@@ -137,6 +172,11 @@ exports.chooseModules = function (userMail, modules, callback) {
     }
   };
 
+  /**
+   * to check if the time id conflict or not
+   * @param list {list} - the list of current module ids
+   * @return {boolean} - to return the status of whether it's conflict or not
+   */
   function checkRepeat(list) {
     var array = [];
     var obj = new Object();
@@ -164,6 +204,12 @@ exports.chooseModules = function (userMail, modules, callback) {
     return result;
   };
 
+  /**
+   * to insert module id to the user table and user module mapping table
+   * @param email {string} - get the user information
+   * @param moduleID {string} - get the module id
+   * @param callback {function} - the result of operations
+   */
   function insertModulesId(email, moduleID, callback) {
     var email = email;
     var sqlStudentID = `SELECT student_id FROM students WHERE email = ?`;
@@ -189,31 +235,4 @@ exports.chooseModules = function (userMail, modules, callback) {
   }
 };
 
-// exports.delModules = function (modulesID, callback) {
-//   var modulesID = modulesID;
-//   var sqlDeleteModuleIDSMM = `DELETE FROM student_modules_mapping WHERE modules_id = ?`;
-//   var sqlDeleteModuleM = `DELETE FROM modules WHERE module_id = ?`;
-//   var sqlDeleteModuleMT = `DELETE FROM modules_time WHERE module_id = ?`;
-//
-//   db.run(sqlDeleteModuleIDSMM, modulesID, function (err) {
-//     if (err) {
-//       callback(err);
-//     } else {
-//       db.run(sqlDeleteModuleM, modulesID, function (err) {
-//         if (err) {
-//           callback(err);
-//         } else {
-//           db.run(sqlDeleteModuleMT, modulesID, function (err) {
-//             if (err) {
-//               callback(err);
-//             } else {
-//               console.log('Delete the column');
-//             }
-//           });
-//           callback();
-//         }
-//       })
-//     }
-//   })
-// };
 
